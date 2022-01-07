e73502e988c8dffda268a3086d3353b9da57df9f
carlosbravo1408 <63279126+carlosbravo1408@users.noreply.github.com>
2022-01-07 01:17:59 -0500

    first commit: Proyecto creado

---
+++ HibernarPC/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace HibernarPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.rdb_hibernar = new System.Windows.Forms.RadioButton();
+            this.rdb_reiniciar = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 13);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Minutos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // rdb_hibernar
+            // 
+            this.rdb_hibernar.AutoSize = true;
+            this.rdb_hibernar.Checked = true;
+            this.rdb_hibernar.Location = new System.Drawing.Point(125, 1);
+            this.rdb_hibernar.Name = "rdb_hibernar";
+            this.rdb_hibernar.Size = new System.Drawing.Size(65, 17);
+            this.rdb_hibernar.TabIndex = 3;
+            this.rdb_hibernar.TabStop = true;
+            this.rdb_hibernar.Text = "Hibernar";
+            this.rdb_hibernar.UseVisualStyleBackColor = true;
+            this.rdb_hibernar.CheckedChanged += new System.EventHandler(this.rdb_hibernar_CheckedChanged);
+            // 
+            // rdb_reiniciar
+            // 
+            this.rdb_reiniciar.AutoSize = true;
+            this.rdb_reiniciar.Location = new System.Drawing.Point(125, 24);
+            this.rdb_reiniciar.Name = "rdb_reiniciar";
+            this.rdb_reiniciar.Size = new System.Drawing.Size(66, 17);
+            this.rdb_reiniciar.TabIndex = 3;
+            this.rdb_reiniciar.Text = "Reiniciar";
+            this.rdb_reiniciar.UseVisualStyleBackColor = true;
+            this.rdb_reiniciar.CheckedChanged += new System.EventHandler(this.rdb_reiniciar_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(276, 44);
+            this.Controls.Add(this.rdb_reiniciar);
+            this.Controls.Add(this.rdb_hibernar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hibernar en...";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton rdb_hibernar;
+        private System.Windows.Forms.RadioButton rdb_reiniciar;
+    }
+}
+
---
+++ HibernarPC/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace HibernarPC
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        bool condition = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (condition)
+            {
+                timer1.Enabled = false;
+                button1.Text = "Aceptar";
+                numericUpDown1.Enabled = true;
+                rdb_hibernar.Enabled = true;
+                rdb_reiniciar.Enabled = true;
+            }
+            else 
+            {
+                timer1.Enabled = true;
+                i = 0;
+                timer1.Start();
+                button1.Text = "Cancelar";
+                numericUpDown1.Enabled = false;
+                rdb_hibernar.Enabled = false;
+                rdb_reiniciar.Enabled = false;
+                this.WindowState = FormWindowState.Minimized;
+            }
+            condition = !condition;
+
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(i==numericUpDown1.Value*60)
+            {
+                if (rdb_hibernar.Checked == true)
+                {
+                    Process.Start("shutdown", "/h");
+                    this.Close();
+                }
+                else
+                {
+                    Process.Start("shutdown", "/r");
+                    this.Close();
+                }
+            }
+            TimeSpan sec = TimeSpan.FromSeconds((double)numericUpDown1.Value*60-i);
+            string str = sec.ToString(@"hh\:mm\:ss");
+            Text = str;
+            i++;
+
+        }
+
+        private void rdb_hibernar_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Text = "Hibernar en: ";
+        }
+
+        private void rdb_reiniciar_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Text = "Reiniciar en: ";
+        }
+    }
+}
